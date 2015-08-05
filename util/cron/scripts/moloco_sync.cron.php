0a360dba5d2e003951e0ea6e13132324123f08eb
Gerrit <g@sloan.mit.edu>
2015-08-05 19:37:05 -0400

    Cleanup

--- util/cron/scripts/moloco_sync.cron.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-`python /var/www/twat/TwitterFollowBot-master/moloco_sync.py 2>&1`;
-
-?>
--- util/cron/scripts/moloco_tweet.cron.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo `python /var/www/twat/TwitterFollowBot-master/moloco_rt.py 2>&1`;
-
-?>

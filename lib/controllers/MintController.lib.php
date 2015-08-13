0b386ab1e432d78a6512c9f25902186a5ed86c2f
Gerrit <g@sloan.mit.edu>
2015-08-13 14:41:31 -0400

    Mint

--- lib/controllers/MintController.lib.php
+++ lib/controllers/MintController.lib.php
@@ -24,18 +24,22 @@ class MintController extends Controller {
 	
 	public function run() {
 		$G = new Github;
-		$G->load_user();
 
 		$req_url = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 		$path = parse_url($req_url, PHP_URL_PATH);
 		$paths = explode("/", $path);
 		$project_name = $paths[2].'/'.$paths[3];
 
+		$this->state['project_name'] = $project_name;
 		
 		$this->state['repo'] = $G->load_stored_repo($project_name);
 		if($this->state['repo']['status'] == 4) {
+			if(!$this->state['repo']['public']) {
+				$G->load_user();
+			}
 			$G->load_coin_owners($project_name);
 			$this->state['date_minted'] = $G->date_minted;
+			$this->state['last_commit'] = strftime("%b %d", strtotime(query_grab("SELECT max(commit_date) FROM commit_log WHERE repo = '".$this->state['repo']['name']."'")));
 			$this->state['asset_hash'] = $G->asset_hash;
 			$this->state['cal'] = $G->schedule;
 			$this->state['contributors'] = $G->contributors;
@@ -43,10 +47,11 @@ class MintController extends Controller {
 			$this->state['today'] = $G->today;
 
 		} else {
+		
+		$G->load_user();
 		$BTC = new Bitcoin;	
 		$BTC->generate_address($project_name);
 
-		$this->state['project_name'] = $project_name;
 		$this->state['btc_address'] = $BTC->input_address;
 		$this->state['status'] = $BTC->status;
 		$this->state['hash'] = $BTC->transaction_hash;

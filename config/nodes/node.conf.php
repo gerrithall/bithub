eac388cb879114e4656fed28ab889c59048b1361
Gerrit <g@sloan.mit.edu>
2015-08-19 02:44:25 -0400

    easy install

---
+++ config/nodes/node.conf.php
@@ -0,0 +1,20 @@
+<?php
+
+define('DBSERVER','localhost');
+define('DBUSER','username');
+define('DBPASS','password');
+define('DBPRIMARY','bithub');
+
+
+define('SITE_PATH','/path/to/source');
+
+# Install the latest version from http://smarty.net/
+define('SMARTY_PATH','/path/to/smarty/');
+
+define('DOMAIN', 'your.local.domain');
+define('SITE_URL','http://'.DOMAIN);
+
+#define('SYSTEM_LOG_PATH','/home/zcor/logs/system.log');
+#define('QUERY_LOG_PATH','/home/zcor/logs/query.log');
+
+?>
